dd7ed095515037f49cc1f2cb9a2b6b2547944d1f
Lucas Emanoel <126834052+Luska0@users.noreply.github.com>
2023-07-03 15:29:58 -0300

    Update lista07q2.cpp

--- lista07/lista07q2.cpp
+++ lista07/lista07q2.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main() {
     string input;
     cout << "Digite uma string: ";
-    getline(std::cin, input);
+    getline(cin, input);
 
     if (input.length() >= 4) {
         string fourLetters = input.substr(0, 4);
