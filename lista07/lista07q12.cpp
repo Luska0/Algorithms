a49575d22824a210cb13eb8c0b70afc4b930861b
Lucas Emanoel <126834052+Luska0@users.noreply.github.com>
2023-07-04 14:36:51 -0300

    Add files via upload

---
+++ lista07/lista07q12.cpp
@@ -0,0 +1,38 @@
+/*Aluno: Lucas Emanoel Ribeiro Santana
+  Turma: IF23
+  Professor: Josenalde Barbosa de Oliveira
+                  Lista de Exercícios 7
+                        Questão 12
+12) Escreva um programa que recebe uma string S e dois valores inteiros não negativos i e j. Em
+seguida, imprima os caracteres contidos no segmento que vai de i a j da string S.
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string S;
+    int i, j;
+
+    // Solicita a entrada da string S
+    cout << "Digite a string S: ";
+    getline(std::cin, S);
+
+    // Solicita a entrada dos valores inteiros i e j
+    cout << "Digite os valores inteiros i e j: ";
+    cin >> i >> j;
+
+    // Verifica se os valores de i e j são válidos
+    if (i < 0 || j < 0 || i > j || j >= S.length()) {
+        cout << "Valores inválidos de i e j." << endl;
+        return 0;
+    }
+
+    // Imprime os caracteres contidos no segmento que vai de i a j
+    cout << "Segmento de S que vai de i a j: ";
+    for (int k = i; k <= j; k++) {
+        cout << S[k];
+    }
+    cout << endl;
+
+    return 0;
+}
